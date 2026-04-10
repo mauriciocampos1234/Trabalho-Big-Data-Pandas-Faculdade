05a94814dbf63baf4732d89a097519e782d6bca7
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2026-04-10 13:38:29 -0300

    Passo 7: O desafio da coluna 'Date' (Parte 1)

--- trabalho_pratico.py
+++ trabalho_pratico.py
@@ -3,11 +3,14 @@ import pandas as pd
 # 1. Ler o CSV (usando as configurações que já validamos)
 df = pd.read_csv('data.csv', sep=';', engine='python', encoding='utf-8')
 
-# 2. Verificar a importação
-print("--- Info Original ---")
-df.info()
-print("\n--- Primeiras Linhas ---")
-print(df.head())
+# 1. Substituir nulos por '1900/01/01'
+df['Date'] = df['Date'].fillna('1900/01/01')
 
-# 3. Criar uma variável com a CÓPIA dos dados
-df_limpo = df.copy()
+print("\n--- Verificação: Coluna Date com 1900/01/01 ---")
+print(df.loc[[22]]) # A linha 22 era a que tinha NaN
+
+# 2. Tentar transformar em datetime (ISSO VAI GERAR ERRO)
+try:
+    df['Date'] = pd.to_datetime(df['Date'], format='%Y/%m/%d')
+except Exception as e:
+    print(f"\n❌ ERRO ESPERADO ENCONTRADO: {e}")
