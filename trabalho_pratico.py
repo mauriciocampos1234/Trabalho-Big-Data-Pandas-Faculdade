c9b13ab945c9394a5ffeaba779c676a9b9ecef9d
mauriciocampos1234 <mauriciocampospaypal@gmail.com>
2026-04-10 13:39:20 -0300

    Passo 6: Tratando a coluna 'Calories'

--- trabalho_pratico.py
+++ trabalho_pratico.py
@@ -3,14 +3,8 @@ import pandas as pd
 # 1. Ler o CSV (usando as configurações que já validamos)
 df = pd.read_csv('data.csv', sep=';', engine='python', encoding='utf-8')
 
-# 1. Substituir nulos por '1900/01/01'
-df['Date'] = df['Date'].fillna('1900/01/01')
+# Substituir valores nulos por 0 na coluna 'Calories'
+df['Calories'] = df['Calories'].fillna(0)
 
-print("\n--- Verificação: Coluna Date com 1900/01/01 ---")
-print(df.loc[[22]]) # A linha 22 era a que tinha NaN
-
-# 2. Tentar transformar em datetime (ISSO VAI GERAR ERRO)
-try:
-    df['Date'] = pd.to_datetime(df['Date'], format='%Y/%m/%d')
-except Exception as e:
-    print(f"\n❌ ERRO ESPERADO ENCONTRADO: {e}")
+print("\n--- Verificação: Coluna Calories (Linhas 18 e 28) ---")
+print(df.loc[[18, 28]])
